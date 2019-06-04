ffc3a37c76d1d23629440384b6dc1f17ac45bd7b
jmhill91 <john.m.hill91@gmail.com>
2019-06-03 21:03:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = 14 % 5
+const max = Math.max(1,20)
