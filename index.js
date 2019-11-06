40e4a3fec8cc2b33378c8b14198fc3c5a6255f77
askoenig <askoenig94@gmail.com>
2019-11-06 15:52:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 15.5;
+let num2 = 4;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 1000) + 1;
+let mod = 20 % 8;
+let max = Math.max(1, 6, 14, 7, 20);
+
+// const var1 = 2;
+// const var2 = 31;
+// const multiply = var1 * var2;
+// const random = Math.floor(Math.random() * 1000) + 1;
+// const mod = 16 % 6;
+// const max = Math.max(5, 10, 15, 20);
